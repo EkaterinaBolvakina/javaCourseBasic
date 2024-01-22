ddfd8af13db4ddb3a247c80ea2c624857f7d8d5b
EkaterinaBolvakina <ekaterina.bolvakina@gmail.com>
2024-01-22 19:37:26 +0100

    Commit1

--- src/homeworks/homework_04/task_03/Product.java
+++ src/homeworks/homework_04/task_03/Product.java
@@ -11,13 +11,15 @@ public class Product {
     }
     public void purchaseResult(){
         int sumAfterDiscount = (priceProductA + priceProductB) - discount;
-        System.out.println("Total after discount: "+ sumAfterDiscount);
-        System.out.println("Discount: " + discount);
+        System.out.println("Purchase price: ");
+        System.out.println("Total after discount: "+ sumAfterDiscount+ " RUB");
+        System.out.println("Discount: " + discount+ " RUB");
     }
 // OR other variant if necessary:
     public void purchaseResult2 (int priceProductA, int priceProductB, int discount ){
         int sumAfterDiscount = (priceProductA + priceProductB) - discount;
-        System.out.println("Total after discount: "+ sumAfterDiscount);
-        System.out.println("Discount: " + discount);
+        System.out.println("Purchase price: ");
+        System.out.println("Total after discount: "+ sumAfterDiscount+ " RUB");
+        System.out.println("Discount: " + discount+ " RUB");
     }
 }
