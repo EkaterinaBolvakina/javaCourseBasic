b364de5bf86709155c82ef29ac91be83c3063dca
EkaterinaBolvakina <ekaterina.bolvakina@gmail.com>
2024-01-27 20:05:36 +0100

    Commit1

--- src/homeworks/homework_09/task_02/Random.java
+++ src/homeworks/homework_09/task_02/Random.java
@@ -20,14 +20,15 @@ public class Random {
            Dafür kann man diese Formel nutzen:
            min + random() * ( max - min + 1) oder
            random() * ( max - min + 1) + min
+           !!!wenn aber min = 0, dann: random() * ( max - min ) + min
          */
         int min = 0;
         int max = 100;
         //- Запишите в 4 переменные случайные числа от 0 до 100
-        long num1 = Math.round(Math.random() * (max - min + 1) + min);
-        long num2 = Math.round(Math.random() * (max - min + 1) + min);
-        long num3 = Math.round(Math.random() * (max - min + 1) + min);
-        long num4 = Math.round(Math.random() * (max - min + 1) + min);
+        long num1 = Math.round(Math.random() * (max - min ) + min);
+        long num2 = Math.round(Math.random() * (max - min ) + min);
+        long num3 = Math.round(Math.random() * (max - min ) + min);
+        long num4 = Math.round(Math.random() * (max - min ) + min);
         //- Выведите все 4 на экран
         System.out.println("1st number: "+num1);
         System.out.println("2nd number: "+num2);
