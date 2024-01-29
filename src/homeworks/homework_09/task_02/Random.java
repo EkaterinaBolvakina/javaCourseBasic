4569edc0130c68fbb0cb16bde7d453e585292591
EkaterinaBolvakina <ekaterina.bolvakina@gmail.com>
2024-01-29 19:46:00 +0100

    Commit1

--- src/homeworks/homework_09/task_02/Random.java
+++ src/homeworks/homework_09/task_02/Random.java
@@ -23,23 +23,36 @@ public class Random {
            !!!wenn aber min = 0, dann: random() * ( max - min ) + min
          */
         int min = 0;
-        int max = 100;
+        int max = 99;
         //- Запишите в 4 переменные случайные числа от 0 до 100
-        long num1 = Math.round(Math.random() * (max - min ) + min);
-        long num2 = Math.round(Math.random() * (max - min ) + min);
-        long num3 = Math.round(Math.random() * (max - min ) + min);
-        long num4 = Math.round(Math.random() * (max - min ) + min);
+        long num1 = Math.round(Math.random() * (max - min + 1) + min);
+        long num2 = Math.round(Math.random() * (max - min + 1) + min);
+        long num3 = Math.round(Math.random() * (max - min + 1) + min);
+        long num4 = Math.round(Math.random() * (max - min + 1) + min);
         //- Выведите все 4 на экран
-        System.out.println("1st number: "+num1);
-        System.out.println("2nd number: "+num2);
-        System.out.println("3rd number: "+num3);
-        System.out.println("4th number: "+num4);
+        System.out.println("1st number: " + num1);
+        System.out.println("2nd number: " + num2);
+        System.out.println("3rd number: " + num3);
+        System.out.println("4th number: " + num4);
         //- Программа должна определить максимальное из этих четырех чисел
-        long maxOfNumbersOneAndTwo = Math.max(num1,num2);
-        long maxOfNumbersThreeAndFour = Math.max(num3,num4);
-        long maxOfNumbers = Math.max(maxOfNumbersOneAndTwo,maxOfNumbersThreeAndFour);
+        //Variant 1 with Math.max()
+        long maxOfNumbersOneAndTwo = Math.max(num1, num2);
+        long maxOfNumbersThreeAndFour = Math.max(num3, num4);
+        long maxOfNumbers = Math.max(maxOfNumbersOneAndTwo, maxOfNumbersThreeAndFour);
         //- Результат вывести на экран
-        System.out.println("Max of these 4 numbers: " +maxOfNumbers);
+        System.out.println("Max of these 4 numbers (Math.max): " + maxOfNumbers);
 
+        //Variant 2 with if-else:
+        //- Программа должна определить максимальное из этих четырех чисел with if-else:
+        if ((num4 > num1) && (num4 > num2) && (num4 > num3)){
+                    System.out.println("Max of these 4 numbers (IF-ELSE): " + num4);
+        } else if ((num3 > num1) && (num3 > num2) && (num3 > num4) ){
+                    System.out.println("Max of these 4 numbers (IF-ELSE): " + num3);
+        } else if ((num2 > num1) && (num2 > num3) && (num2 > num4)){
+                    System.out.println("Max of these 4 numbers (IF-ELSE): " + num2);
+                }else {
+                     System.out.println("Max of these 4 numbers (IF-ELSE): " + num1);
+                 }
+        }
     }
-}
+
