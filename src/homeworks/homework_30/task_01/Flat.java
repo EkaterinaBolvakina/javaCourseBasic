d8fa72c2695fda27a64665b29316f49c9a418fb0
EkaterinaBolvakina <ekaterina.bolvakina@gmail.com>
2024-02-26 15:55:22 +0100

    Commit1

---
+++ src/homeworks/homework_30/task_01/Flat.java
@@ -0,0 +1,37 @@
+package homeworks.homework_30.task_01;
+
+public class Flat implements Comparable<Flat>{
+    private int area;
+    private int roomCount;
+
+    public Flat(int area, int roomCount) {
+        this.area = area;
+        this.roomCount = roomCount;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public int getRoomCount() {
+        return roomCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Flat{" +
+                "area=" + area +
+                ", roomCount=" + roomCount +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Flat flatToCompare) {
+        int result = roomCount - flatToCompare.roomCount;
+
+        if ( result == 0){
+            result = area - flatToCompare.area;
+
+        }return result;
+    }
+}
