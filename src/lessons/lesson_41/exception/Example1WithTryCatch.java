a1ea19bc14236413335ff95ee0e552744bf11e15
EkaterinaBolvakina <ekaterina.bolvakina@gmail.com>
2024-03-13 19:06:04 +0100

    Commit1

---
+++ src/lessons/lesson_41/exception/Example1WithTryCatch.java
@@ -0,0 +1,27 @@
+package lessons.lesson_41.exception;
+
+import lesson_06.code.lessoncode.scanner.UserInput;
+
+public class Example1WithTryCatch {
+    public static void main(String[] args) {
+        UserInput ui = new UserInput();
+
+        int x = ui.inputInteger("Введите первое число");
+        int y = ui.inputInteger("Введите второе число");
+
+
+        try {
+            System.out.println("результат деления: " + divide(x, y));
+        } catch (Exception exception){
+            System.out.println("произошла ошибка " + exception.getMessage());
+        }
+
+
+        System.out.println("Программа завершила свою работу");
+
+    }
+
+    static double divide(int x, int y){
+        return x / y;
+    }
+}
---
+++ src/lessons/lesson_41/exception/validation/ValidationExample.java
@@ -0,0 +1,23 @@
+package lessons.lesson_41.exception.validation;
+
+import lesson_06.code.lessoncode.scanner.UserInput;
+
+public class ValidationExample {
+    public static void main(String[] args) {
+        UserInput ui = new UserInput();
+
+        String userName = ui.inputText("Введите ваше имя: ");
+
+        System.out.println(checkUserName(userName));
+
+    }
+
+    static boolean checkUserName(String name){
+        if (name.length() < 3) return false;
+        if (name.length() > 15) return false;
+        if (name.contains("&")) return false;
+
+        return true;
+
+    }
+}
